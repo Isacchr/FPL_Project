ab25d7bea21c56051933773c6b378adb6d830e35
Isacchr <isac.christiansson@gmail.com>
2024-03-15 17:47:08 +0100

    started base of draft page

---
+++ src/components/DraftBase.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Grey_shirt from "../assets/images/Grey_shirt.png"
+</script>   
+
+<script>
+    export default {
+    data() {
+        return {
+        GreyShirt: Grey_shirt
+        };
+    },
+    methods: {},
+    };
+</script>
+
+<template>
+    <div class="bg-white w-[90%] h-[80%] m-auto text-center opacity-85 rounded-md">
+        <p>Yess</p>
+
+        <div class="flex">
+
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+        <img :src="GreyShirt" class="w-[8%] h-auto">
+
+        </div>
+
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
-import Title from "../components/Title.vue"
+import Title from "../components/Title.vue";
+import DraftBase from "@/components/DraftBase.vue";
 </script>
 
 <template>
 
-  <div class=" h-screen bg-[url('/src/assets/images/FPL_Background.jpg')] bg-cover">    
+  <div class=" h-screen bg-[url('/src/assets/images/FPL_Background.jpg')] bg-cover">
     <Title /> 
+    <DraftBase />
   </div>
 
 </template>
